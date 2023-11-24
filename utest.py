d1d3e227daa0d86b78ea67e7a4be3825a99ef4bc
Sina Akbarian <39638602+sinaakbarian@users.noreply.github.com>
2023-11-24 15:38:45 -0500

    Update utest.py

--- utest.py
+++ utest.py
@@ -1,26 +1,23 @@
-# test code
-
-import unittest
 import numpy as np
 from main import SimpleLinearModel
 
 def test_model_prediction():
-      # Create a simple dataset
-      X_train = np.array([[1], [2], [3]])
-      y_train = np.array([2, 4, 6])
+    # Create a simple dataset
+    X_train = np.array([[1], [2], [3]])
+    y_train = np.array([2, 4, 6])
 
-      # Instantiate the model
-      model = SimpleLinearModel()
+    # Instantiate the model
+    model = SimpleLinearModel()
 
-      # Fit the model
-      model.fit(X_train, y_train)
+    # Fit the model
+    model.fit(X_train, y_train)
 
-      # Test the prediction
-      X_test = np.array([[4]])
-      prediction = model.predict(X_test)
+    # Test the prediction
+    X_test = np.array([[4]])
+    prediction = model.predict(X_test)
 
-      # Assert that the prediction is close to the expected value
-      self.assertAlmostEqual(prediction[0], 8, places=2, msg="Model prediction failed for X_test=[4]")
+    # Assert that the prediction is close to the expected value
+    assert np.allclose(prediction, 8, atol=1e-2), "Model prediction failed for X_test=[4]"
 
 if __name__ == '__main__':
     test_model_prediction()
