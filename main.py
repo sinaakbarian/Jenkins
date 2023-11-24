08964464a805fa2d5406be7697b2025b0eab54d8
Sina Akbarian <39638602+sinaakbarian@users.noreply.github.com>
2023-11-24 14:20:05 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# simple ml model
+
+from sklearn.linear_model import LinearRegression
+
+class SimpleLinearModel:
+    def __init__(self):
+        self.model = LinearRegression()
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
